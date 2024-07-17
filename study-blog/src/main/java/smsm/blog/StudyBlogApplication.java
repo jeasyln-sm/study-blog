84284d1e6f5235859b10bff475dfde82a49353f4
chobi <xyz11357@gmail.com>
2024-07-17 09:21:55 +0900

    first commit

---
+++ study-blog/src/main/java/smsm/blog/StudyBlogApplication.java
@@ -0,0 +1,13 @@
+package smsm.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBlogApplication.class, args);
+	}
+
+}
---
+++ study-blog/src/test/java/smsm/blog/StudyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package smsm.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
